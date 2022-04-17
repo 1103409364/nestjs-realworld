77ddde182786a415c1667156567e0e3bad4c9d14
1103409364 <1103409364@qq.com>
2022-04-17 16:15:58 +0800

    fix: mysql
    Signed-off-by: 1103409364 <1103409364@qq.com>

--- src/main.ts
+++ src/main.ts
@@ -18,5 +18,7 @@ async function bootstrap() {
   SwaggerModule.setup('/docs', app, document);
 
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  
 }
 bootstrap();
